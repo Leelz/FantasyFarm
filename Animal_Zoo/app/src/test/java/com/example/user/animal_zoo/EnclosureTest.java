4d7eef00b3e03d898b4c921ddf076979c93b0cd8
Leelz <adam.leel93@gmail.com>
2016-12-18 20:05:40 +0000

    started building the test for Enclosure

--- Animal_Zoo/app/src/test/java/com/example/user/animal_zoo/EnclosureTest.java
+++ Animal_Zoo/app/src/test/java/com/example/user/animal_zoo/EnclosureTest.java
@@ -1,7 +1,23 @@
 package com.example.user.animal_zoo;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by user on 18/12/2016.
  */
 public class EnclosureTest {
+
+    private Enclosure enclosure;
+
+    @Before
+    public void Before()    {
+        enclosure = new Enclosure("AQUARIUM");
+    }
+
+    @Test
+    public void testGetEnclosureType(){
+        assertEquals("AQUARIUM", enclosure.getType());
+    }
+
+    
 }

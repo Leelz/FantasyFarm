6a42a3114df8a547dfb24aba4631a2933adc5ab8
Leelz <adam.leel93@gmail.com>
2016-12-18 20:00:28 +0000

    started building the zoo class! Witgetter and add enclosure to zoo functions.

--- Animal_Zoo/app/src/main/java/com/example/user/animal_zoo/Zoo.java
+++ Animal_Zoo/app/src/main/java/com/example/user/animal_zoo/Zoo.java
@@ -1,7 +1,25 @@
 package com.example.user.animal_zoo;
+import java.util.ArrayList;
 
 /**
  * Created by user on 18/12/2016.
  */
+
 public class Zoo {
+
+    private ArrayList<Enclosure> allEnclosures;
+
+    public Zoo(ArrayList<Enclosure> allEnclosures) {
+        this.allEnclosures = new ArrayList<Enclosure>(allEnclosures)
+    }
+
+    public ArrayList<Enclosure> getAllEnclosures() {
+        return allEnclosures;
+    }
+
+    public void addEnclosureToZoo(Enclosure enclosure) {
+        allEnclosures.add(enclosure);
+    }
+
+
 }
